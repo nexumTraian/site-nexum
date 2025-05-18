96fdd0f9db74fcc70a3cb6d2d8a53f1a832b9071
nexumTraian <tbrehui@gmail.com>
2025-05-18 20:47:52 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Scroll animat între secțiuni (pentru single-page)
+// Scroll animat între secțiuni
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   anchor.addEventListener('click', function (e) {
     e.preventDefault();
